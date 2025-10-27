9bf70afd949ea2043498b17c536d04e2c0b9a693
DiegoJsH <diego.palominos2006@gmail.com>
2025-10-26 21:26:08 -0500

    modulo pacientes arreglado

--- src/main/java/com/psicoclinic/psicoclinic/controllers/PacienteController.java
+++ src/main/java/com/psicoclinic/psicoclinic/controllers/PacienteController.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.psicoclinic.psicoclinic.models.PacienteModel;
 import com.psicoclinic.psicoclinic.services.PacienteService;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/pacientes")
 public class PacienteController {
